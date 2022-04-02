2c38675729d59333a91a2382c5005e2b3a28f624
Jason Davis <tripod6720@gmail.com>
2022-04-02 13:04:41 -0400

    Repo Initialized, ignore file added, comment start
    Comments for planning were added to the console app. Questions
    for Chris placed in comments.
    
    Other notes and thoughts:
    -Start writing, refactor if necessary/helpful before you submit

---
+++ qdxExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace qdxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Randomly Generate a 4 digit number; Each digit "between numbers 1 and 6" 
+            //Thoughts: Store in an Array. Easy to index and compare. Easy to loop through
+            //Assuming 1-6 inclusive, be sure to ask before you submit your final program
+
+
+
+
+            //allow player to input a combination of 4 digits
+            //error check inputs
+            //question: should the user be able to choose only 1 of each digit? 
+            //question: should I ask for a 4 digit number from the user or only one at a time.
+            //Note: Confirm with Quadax before you submit. Working on the assumption that they will pick one at a time and only one per digit
+
+
+            //compare to number and return
+            //  count up included and in the right spot for a +
+            //  count up included and not in the right spot for a -
+            //  return result
+                 //+ signs come first, - signs second
+
+            //Allow 10 Chances
+               
+            //Example: 3512
+            //Guess: 5318 should return +--
+            //Next Guess: 1356 should return +--
+            //Next Guess: 3152 should return +++-
+
+
+
+
+
+
+
+
+        }
+    }
+}
