e4a66523eaa96642feb439686962b8481114024d
Jason Davis <tripod6720@gmail.com>
2022-04-02 14:33:11 -0400

    Target Number Generated - Questions Remain
    Assuming that the target number should not have repeated digits
    Number randomly generated with the caveat that a single option
    for a digit can only be used once. 1 to 6 inclusive.

--- qdxExercise/Program.cs
+++ qdxExercise/Program.cs
@@ -6,12 +6,50 @@ namespace qdxExercise
     {
         static void Main(string[] args)
         {
-            
-            //Randomly Generate a 4 digit number; Each digit "between numbers 1 and 6" 
+
+            //---------------Randomly Generate a 4 digit number; Each digit "between numbers 1 and 6" ------------------
+            //Questions:
+            //Do all digits need to be unique?
+                //Based on mastermind, assuming yes. 
+            //Are the 1-6 digits inclusive?
+                //Based on mastermind, assuming yes.
+
             //Thoughts: Store in an Array. Easy to index and compare. Easy to loop through
-            //Assuming 1-6 inclusive, be sure to ask before you submit your final program
+            
+            
+            //Instantiate an array to hold the winning number  
+            int[] numberWinning = new int[4];
+
+            /*
+            //Populate array - Duplicates Allowed
+            for (int i = 0; i < 4; i++)
+            {
+                numberWinning[i] = new Random().Next(1, 7);
+            }
+            */
+
+            
+            //Populate array - Duplicates not Allowed
+            //Running With This Plan
+            int[] digits = new int[] { 1, 2, 3, 4, 5, 6 };
+            int randomDigit = new Random().Next(0, 6);
+           
+            for (int i = 0; i < 4; i++)
+            {
+                while (digits[randomDigit] == 0)
+                {
+                    randomDigit = new Random().Next(0, 6);
+                }
+                numberWinning[i] = digits[randomDigit];
+                digits[randomDigit] = 0;
+            }
 
 
+
+
+            //Preview Number
+            string checkJoin = String.Join("", Array.ConvertAll<int, String>(numberWinning, Convert.ToString));
+            Console.WriteLine(checkJoin);
 
 
             //allow player to input a combination of 4 digits
@@ -25,10 +63,10 @@ namespace qdxExercise
             //  count up included and in the right spot for a +
             //  count up included and not in the right spot for a -
             //  return result
-                 //+ signs come first, - signs second
+            //+ signs come first, - signs second
 
             //Allow 10 Chances
-               
+
             //Example: 3512
             //Guess: 5318 should return +--
             //Next Guess: 1356 should return +--
