2d0b406de2995896dab2cab9d3d7521e9e459c79
Jason Davis <tripod6720@gmail.com>
2022-04-03 22:29:27 -0400

    Deliverable. Will have fun with this in the future
    Hello to anyone at Quadax that reads commit messages! I enjoyed the
    coding challenge and hope that we can talk again in the future.
    I will be returning to this code from time to time to improve it, so
    if you find yourself bored and with free time, please feel free to
    return.
    
    I hope it brought at least a smile or two in the process!
    
    Congratulations on the 2022 Best in KLAS! It would be an honor
    to earn a place on your team.

--- qdxExercise/Program.cs
+++ qdxExercise/Program.cs
@@ -13,19 +13,24 @@ namespace qdxExercise
 
         static void Main(string[] args)
         {
+            Greeting();
+
 
             int[] winningNumber = generateWinningNumber();
-            
+            string winningNumString = String.Join("", Array.ConvertAll<int, String>(winningNumber, Convert.ToString));
+            bool win = false;
             int[] userGuess = new int[4];
+
             int attemptCount = 1;
+
             string results = "";
 
             List<int[]> previousGuessList = new List<int[]>();
             List<string> previousResults = new List<string>();
 
 
+
             while (attemptCount <= 10)
-            //while (userGuess != winningNumber && attemptCount <= 10)
             {
                 userGuess = userNumberChoice(previousGuessList, previousResults);
                 previousGuessList.Add(userGuess);
@@ -43,183 +48,221 @@ namespace qdxExercise
                     Console.WriteLine("Winner!");
                     Console.WriteLine("Congratulation, Press Enter To Exit Program");
                     Console.ReadLine();
+                    win = true; 
                     break;
-
                 }
-
-              //  Console.WriteLine($"Press Enter for Guess {attemptCount}");
-               // Console.ReadLine();
                 Console.Clear();
 
                 displayPreviousResults(previousGuessList, previousResults);
 
-                Console.WriteLine($"Guess {attemptCount}");
+                if (attemptCount < 11)
+                {
+                    Console.WriteLine($"Guess {attemptCount}");
+                }
             }
+            if (!win)
+            { 
                 Console.WriteLine("Sorry! Thanks for Playing! Please Come Again!");
-            
-        }
-
-
-
-
-        //Changing to Methods, Cleaning Up. 
-
-        //---------------Randomly Generate a 4 digit number; Each digit "between numbers 1 and 6" ------------------
-        static int[] generateWinningNumber()
-        {
-            //Instantiate an array to hold the winning number  
-            int[] numberWinning = new int[4];
-
-            //Populate array - Duplicates not Allowed
-            int[] digits = new int[] { 1, 2, 3, 4, 5, 6 };
-            int randomDigit = new Random().Next(0, 6);
-
-            for (int i = 0; i < 4; i++)
-            {
-                //Zero takes the place of used digits, loop continues until it finds an unused digit
-                while (digits[randomDigit] == 0)
-                {
-                    randomDigit = new Random().Next(0, 6);
-                }
-                //Add digit to array that represents the winning number
-                numberWinning[i] = digits[randomDigit];
-
-                //Set used digit to 0;
-                digits[randomDigit] = 0;
+                Console.WriteLine($"The correct sequence was: {winningNumString}");
+                System.Threading.Thread.Sleep(5000);
             }
-            return numberWinning;
-        }
 
-
-        //Prompts user to select a four digit number, one at a time to mimic the app
-        static int[] userNumberChoice(List<int[]> previousGuess, List<string> previousResult)
-        {
-            //Array to hold the guess; list to choose from
-            int[] numberGuess = new int[4];
-            List<int> digitChoice = new List<int>() { 1, 2, 3, 4, 5, 6 };
-
-            //display current number (digit-by-digit)
-            for (int i = 0; i < 4; i++)
+            //Methods
+            //---------------Randomly Generate a 4 digit number; Each digit "between numbers 1 and 6" ------------------
+            static int[] generateWinningNumber()
             {
-                //Provide Remaining Choice 
+                //Instantiate an array to hold the winning number  
+                int[] numberWinning = new int[4];
 
-                Console.Write("Choose from the Following Digits: ");
+                //Populate array - Duplicates not Allowed
+                int[] digits = new int[] { 1, 2, 3, 4, 5, 6 };
+                int randomDigit = new Random().Next(0, 6);
 
-                foreach (int remainingDigit in digitChoice)
+                for (int i = 0; i < 4; i++)
                 {
-                    Console.Write(remainingDigit);
-                }
-
-                //Prompt for input
-                Console.WriteLine("\nChoose a Digit and Press Enter");
-                Console.Write("Your Number: ");
-
-                foreach (int number in numberGuess)
-                {
-                    if (number != 0)
+                    //Zero takes the place of used digits, loop continues until it finds an unused digit
+                    while (digits[randomDigit] == 0)
                     {
-                        Console.Write(number);
+                        randomDigit = new Random().Next(0, 6);
                     }
+                    //Add digit to array that represents the winning number
+                    numberWinning[i] = digits[randomDigit];
+
+                    //Set used digit to 0;
+                    digits[randomDigit] = 0;
                 }
-
-                bool success = false;
-                int goodValue = 0;
-                success = Int32.TryParse(Console.ReadLine(), out goodValue);
-
-                while (!success || !digitChoice.Contains(goodValue))
-                {
-                    Console.WriteLine("Invalid Input, Please Choose an availabe digit from the list");
-                    success = Int32.TryParse(Console.ReadLine(), out goodValue);
-                }
-
-                numberGuess[i] = goodValue;
-                digitChoice.Remove(numberGuess[i]);
-
-
-                Console.Clear();
-
-                displayPreviousResults(previousGuess, previousResult);
-
-                if (previousGuess.Count > 0)
-                {
-                    Console.WriteLine($"Guess: {previousGuess.Count + 1}");
-                }
-
+                return numberWinning;
             }
-            return numberGuess;
-        }
 
-        //Calculate results of the current guess (Note: Need to fix order: + first, - second, blank last)
-        static string resultsDisplay(int[] userGuess, int[] winningNumber)
-        {
-            string[] results = new string[4];
-            string resultString = "";
 
-            //Compare guess to winning number
-            for (int i = 0; i < userGuess.Length; i++)
+            //Prompts user to select a four digit number, one at a time to mimic the app
+            static int[] userNumberChoice(List<int[]> previousGuess, List<string> previousResult)
             {
-                //Plus sign for each if it is in the right place
-                if (userGuess[i] == winningNumber[i])
+                //Array to hold the guess; list to choose from
+                int[] numberGuess = new int[4];
+                List<int> digitChoice = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+                //display current number (digit-by-digit)
+                for (int i = 0; i < 4; i++)
                 {
-                    results[i] = "+";
-                }
-                //minus sign for each in the answer, but not in the right place
-                else
-                {
-                    foreach (int numWin in winningNumber)
+                    //Provide Remaining Choice 
+
+                    Console.Write("Choose from the Following Digits: ");
+
+                    foreach (int remainingDigit in digitChoice)
                     {
-                        if (numWin == userGuess[i])
+                        Console.Write(remainingDigit);
+                    }
+
+                    //Prompt for input
+                    Console.WriteLine("\nChoose a Digit and Press Enter");
+                    Console.Write("Your Number: ");
+
+                    foreach (int number in numberGuess)
+                    {
+                        if (number != 0)
                         {
-                            results[i] += "-";
+                            Console.Write(number);
+                        }
+                    }
+
+                    bool success = false;
+                    int goodValue = 0;
+                    success = Int32.TryParse(Console.ReadLine(), out goodValue);
+
+                    while (!success || !digitChoice.Contains(goodValue))
+                    {
+                        Console.WriteLine("Invalid Input, Please Choose an availabe digit from the list");
+                        Console.WriteLine("Current Sequence:");
+                        Console.WriteLine("Your Current Sequence: ");
+                        foreach (int number in numberGuess)
+                        {
+                            if (number != 0)
+                            {
+                                Console.Write(number);
+                            }
+                        }
+                        success = Int32.TryParse(Console.ReadLine(), out goodValue);
+
+                    }
+
+                    numberGuess[i] = goodValue;
+                    digitChoice.Remove(numberGuess[i]);
+
+
+                    Console.Clear();
+
+                    displayPreviousResults(previousGuess, previousResult);
+
+                    if (previousGuess.Count > 0)
+                    {
+                        Console.WriteLine($"Guess: {previousGuess.Count + 1}");
+                    }
+
+                }
+                return numberGuess;
+            }
+
+            //Calculate results of the current guess (Note: Need to fix order: + first, - second, blank last)
+            static string resultsDisplay(int[] userGuess, int[] winningNumber)
+            {
+                string[] results = new string[4];
+                string resultString = "";
+
+                //Compare guess to winning number
+                for (int i = 0; i < userGuess.Length; i++)
+                {
+                    //Plus sign for each if it is in the right place
+                    if (userGuess[i] == winningNumber[i])
+                    {
+                        results[i] = "+";
+                    }
+                    //minus sign for each in the answer, but not in the right place
+                    else
+                    {
+                        foreach (int numWin in winningNumber)
+                        {
+                            if (numWin == userGuess[i])
+                            {
+                                results[i] += "-";
+                            }
                         }
                     }
                 }
-            }
-            //Sort '+' first, then '-', then ''
-            Array.Sort(results);
-            Array.Reverse(results);
-            foreach (string result in results)
-            {
-                resultString += result;
-            }
-            //return resultsString for use
-            return resultString;
-        }
-
-        static void displayPreviousResults(List<int[]> previousGuess, List<string> previousResult)
-        {
-            int guessCount = 1;
-
-            if (previousGuess.Count > 0)
-            {
-                for (int j = 0; j < previousGuess.Count; j++)
+                //Sort '+' first, then '-', then ''
+                Array.Sort(results);
+                Array.Reverse(results);
+                foreach (string result in results)
                 {
-                    Console.WriteLine($"Guess {guessCount}");
-                    Console.WriteLine(String.Join("", Array.ConvertAll<int, String>(previousGuess[j], Convert.ToString)));
-                    Console.WriteLine(previousResult[j]);
-                    guessCount++;
+                    resultString += result;
+                }
+                //return resultsString for use
+                return resultString;
+            }
+
+            static void displayPreviousResults(List<int[]> previousGuess, List<string> previousResult)
+            {
+                int guessCount = 1;
+
+                if (previousGuess.Count > 0)
+                {
+                    for (int j = 0; j < previousGuess.Count; j++)
+                    {
+                        Console.WriteLine($"Guess {guessCount}");
+                        Console.WriteLine(String.Join("", Array.ConvertAll<int, String>(previousGuess[j], Convert.ToString)));
+                        Console.WriteLine(previousResult[j]);
+                        guessCount++;
+                    }
                 }
             }
-        }
-        static bool arrayEquality(int[] intArray1, int[] intArray2)
-        {
-            if (intArray1.Length == intArray2.Length)
+            static bool arrayEquality(int[] intArray1, int[] intArray2)
             {
-                for (int i = 0; i < intArray1.Length; i++)
+                int counter = 0;
+
+                if (intArray1.Length == intArray2.Length)
                 {
-                    if (intArray1[i] == intArray2[i])
+                    for (int i = 0; i < intArray1.Length; i++)
+                    {
+                        if (intArray1[i] == intArray2[i])
+                        {
+                            counter++;
+                        }
+                    }
+
+                    if (counter == 4)
                     {
                         return true;
                     }
-
                     else
                     {
                         return false;
                     }
 
                 }
+                return false;
             }
-            return false;
+            static void Greeting()
+            {
+
+                Console.WriteLine("Welcome to NumberMind");
+                Console.WriteLine("---------------------\n");
+
+                Console.WriteLine("Instructions: ");
+                Console.WriteLine("---------------------\n");
+
+                Console.WriteLine("1. Enter numbers from the available choices listed");
+                Console.WriteLine("2. One at a time, choose a number and press enter");
+                Console.WriteLine("Attempt to Match the Hidden Code in 10 Guess or Less");
+                Console.WriteLine("\n\nHave Fun!");
+                Console.WriteLine("\n\nYou can always check my prime number tester to see if the winning sequence is a 4 digit prime!");
+
+                Console.WriteLine("There's a 1061/9000 chance, roughly 12%");
+                Console.WriteLine("Game Begins in 11 (yeah that's prime!) seconds");
+                System.Threading.Thread.Sleep(11000);
+                Console.Clear();
+
+
+            }
         }
     }
 }
